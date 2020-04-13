d75a9cb07cb1c1e005e9f8f0c3dd35eae3784381
Dmitry <admin.marts@gmail.com>
2020-04-13 18:44:26 +0300

    Some interface

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from logger import log
 from config import config
 from graph import Graph
 
-from flask import Flask, request, redirect, session
+from flask import Flask, request, redirect, session, render_template
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F432Q8ssasaz\n\xec]/'
-
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route('/logout')
 def logout():
@@ -28,20 +29,39 @@ def get_plans():
     response3 = graph.get_tasks(config.plan_id)
     raise Exception(response, response2, response3)
 
+@app.route('/test')
+def test():
+    return render_template("index.html")
 
 @app.route('/')
+@app.route('/postTasks')
 def index():
+    tvars={}
+    if 'fullname' in session:
+        tvars['fullname']=session['fullname']
+    else:
+        tvars['fullname']=None
     if not 'refresh_token' in session:
-        return getCode()
+        return render_template("login.html",tvars=tvars,url=getCode())
     if session['refresh_token'] == None:
-        return getCode()
+        return render_template("login.html",tvars=tvars,url=getCode())
         # there is no refresh_token - so let`s get it
     graph = Graph(refresh_token=session['refresh_token'],
                   params=config.get_dict())
     response = graph.get_me()
     if not 'id' in response:
-        return response
+        tvars["text"]="Can`t login"
+        return render_template("error.html",tvars=tvars)
+
     my_id = response['id']
+
+    session['fullname'] = response['displayName']
+    tvars['fullname']=session['fullname']
+    if request.path !='/postTasks':
+        return render_template('index.html',tvars=tvars)
+
+
+
     response = graph.get_tasks(config.plan_id)
     if 'value' in response:
         tasks = {}
@@ -61,11 +81,18 @@ def index():
                 graph.create_task(tmp_task)
                 tasks[task['id']] = task
         if len(tasks) == 0:
-            return 'Sorry there is no tasks for user {}'.format(
+            tvars["text"] =  'Sorry there is no tasks for user {}'.format(
                 config.plan_copy_user_id)
-        return 'Your tasks has been created! ({})'.format(len(tasks))
-
+            return render_template("error.html", tvars=tvars)
+        tvars['success_title']="Done!"
+        tvars['text']='Your tasks has been created! ({})'.format(len(tasks))
+        return render_template("success.html", tvars=tvars)
+    if 'error' in response:
+        tvars["text"]=response['error']['message']
+        return render_template("error.html",tvars=tvars)
     raise Exception(response)
+
+
 
 
 def getCode():
@@ -78,7 +105,7 @@ def getCode():
         "&response_mode=query" \
         "&scope={}" \
         "&state=auth".format(config.tenant_id, config.client_id, config.SCOPES)
-    return redirect(url, code=302)
+    return url
 
 
 @app.route('/setcode')
